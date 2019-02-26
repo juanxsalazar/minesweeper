0e59da565d4546171783c6c9e8e8923b381940a4
Juan <juanxsalazar21@gmail.com>
2019-02-26 16:27:12 -0500

    minesweeper table and css, no logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,108 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <div>
+        <h1>MineSweeper</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td colSpan="8">
+<select>
+  <option value="0">Easy</option>
+  <option value="1">Mid</option>
+  <option value="2">Hard</option>
+</select>
+<button>Restart</button>
+</td>
+</tr>
+<td colSpan="8">Start new game</td>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<td colSpan="8"> Mines Left</td>
+</tbody>
+</table>
+      </div>
+    );
   }
 }
 
